4acc783461e81572773c82b4ae6a76deb4eb57ac
Cvetina7777 <tt70207252@edu1.pgzaimov.com>
2024-04-10 09:17:26 +0300

    Person

---
+++ DefiniraneNaKlass/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiniraneNaKlass
+{
+    public class Person
+    {
+        public string name;
+        public int age;
+        public void Persons() 
+        {
+            Console.WriteLine($"Az sum {this.name} na {age} godini");
+        }
+    }
+}
---
+++ DefiniraneNaKlass/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+
+namespace DefiniraneNaKlass
+{
+    internal class Program
+    {
+        static void Main()
+        {
+             Person person = new Person();
+            Person person1 = new Person();
+            Person person2 = new Person();
+
+            person.name = "Pesho";
+            person.age = 20;
+            person.Persons();
+
+            person1.name = "Gosho";
+            person1.age = 18;
+            person1.Persons();
+
+            person1.name = "Stamat";
+            person1.age = 43;
+            person1.Persons();
+        }
+    }
+}
